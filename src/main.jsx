8497429d9c2607f1b5e805c7365b62c5561b5aba
Todaywyou <srirezekirahayu2424@gmail.com>
2025-05-29 10:40:44 +0700

    Coba bru

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import NavBar from './components/NavBar.jsx'
 import HomePage from './pages/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import AboutSection from "../components/about-section";
 
 export default function HomePage() {
   return (
     <div>
    <NavBar/>
+<AboutSection/>
     </div>
   );
 }
