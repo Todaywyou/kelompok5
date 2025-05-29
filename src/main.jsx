7ecca39044ac4d2400385b3f236c6c0c6ac70d4b
Todaywyou <srirezekirahayu2424@gmail.com>
2025-05-29 10:16:25 +0700

    penambhan instal dan struktur

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import NavBar from './components/NavBar.jsx'
+import HomePage from './pages/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/'element={<HomePage/>} />
+    </Routes>
+    </BrowserRouter>
+   
   </StrictMode>,
 )
 
