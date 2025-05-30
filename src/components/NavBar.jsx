f980228d8ebe29685bba9a6958bd6f335be7da87
Todaywyou <srirezekirahayu2424@gmail.com>
2025-05-30 14:36:38 +0700

    dd

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -75,7 +75,7 @@ function NavBar() {
                   <Link to="/makan">Makanan & Minuman</Link>
                 </li>
                 <li>
-                  <Link to="/katalog#kuliah">Kebutuhan Kuliah</Link>
+                  <Link to="/katalog">Kebutuhan Kuliah</Link>
                 </li>
               </ul>
             </details>
